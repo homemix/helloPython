b40a7285bb2356549e1fa4867ea40b580ff64372
Wolf <kenmutati@gmail.com>
2021-03-31 15:37:41 +0300

    python strings

---
+++ variables/basic_operators.py
@@ -0,0 +1,19 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing the code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float:%f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer:%d" % myint)
+
+""" Standard Data Types"""
+str = 'Hello World!'
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
