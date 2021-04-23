83183357ba194c3f42fab0d7560ee2453a85f3e2
homemix <kenmutati@gmail.com>
2021-04-23 07:55:27 +0300

    file processing

---
+++ files/files.py
@@ -0,0 +1,35 @@
+import os
+
+try:
+    # fileobject = open("app.log", encoding='utf-8')
+    # fileobject = open('content/testfile.txt','a')
+    # fileobject.write("Now the file has more content than before!")
+
+    fileobject = open('content/testfile.txt','w')
+    fileobject.write("Woops! I have deleted the contents in there by accident!")
+
+
+    # print(fileobject.read())
+    # print(fileobject.readline())
+    # print('File name: ', fileobject.name)
+    # print(' opening mode: ',fileobject.mode)
+finally:
+    fileobject.close()
+
+file = open('content/presidents.txt','r+')
+file.seek(2)
+# print(file.readline())
+# print(file.tell())
+
+file.close()
+
+# with open('content/presidents.txt', 'r') as reader:
+#     print(reader.read())
+
+pets_dict = { 'Bob': 3,
+              'Jimmy': 2,
+              'Laika': 3,
+              'Jimmy': 10,
+              'Jack': 3,
+              'Stella': 3,
+              'Nzinga': 7 }
